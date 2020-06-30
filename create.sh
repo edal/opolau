9d04e62d7d3de5e479cc80c06b589af4d677dd11
edal <edal@gft.com>
2020-06-30 15:55:27 +0200

    El archivo de respuestas contiene las respuestas en crudo, tal como se introdujeron y sin quitar espacios. Esto lo hará el validador de respuestas

--- create.sh
+++ create.sh
@@ -2,8 +2,8 @@
 
 while IFS= read -r line; do
     ctrl=$( echo $line | cut -d: -f1  )
-    ctrl=$(tr '[:lower:]' '[:upper:]' <<<"$ctrl")
-    ctrl=${ctrl//[[:space:]]}
+    #ctrl=$(tr '[:lower:]' '[:upper:]' <<<"$ctrl")
+    #ctrl=${ctrl//[[:space:]]}
     text=$( echo $line | cut -d: -f2- )
     
     echo "{
